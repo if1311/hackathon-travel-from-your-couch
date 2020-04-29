48922f189e0c10689af663db7c504a5be5e20ed8
Ioana Alexandra Filip <ioannaf.@Ioanas-MacBook-Pro.local>
2020-04-29 17:33:43 +0300

    background animation (basic) form for homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import GLOBE from "vanta/dist/vanta.globe.min.js";
+import * as THREE from "three";
+import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.vantaRef = React.createRef();
+  }
+  componentDidMount() {
+    this.vantaEffect = GLOBE({
+      el: this.vantaRef.current,
+      THREE: THREE,
+    });
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy();
+  }
+  render() {
+    return <div ref={this.vantaRef} className="vanta"></div>;
+  }
 }
-
 export default App;
