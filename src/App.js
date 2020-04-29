0289d333f8bb5cd9ba5c31b44ff3f52d410bf9bb
char0n9 <59369140+char0n9@users.noreply.github.com>
2020-04-29 19:05:20 +0300

    randombtn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import RandomButton from "./components/RandomButton";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RandomButton array={[1, 2, 3, 4, 5, 6]} />
+    </div>
+  );
 }
 
 export default App;
