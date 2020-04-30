b73a978cb9d58bd6ff8a3ab3c0ffd6f9861b5357
c4misado <adelmanalec@gmail.com>
2020-04-30 11:09:58 +0300

    added arrow buttons to fs player

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -92,12 +92,14 @@ export default class Carousel extends Component {
 						<video width="90%" height="80%" controls onClick={this.handleClick}></video>
 					</div>
 					<div className="controls">
-						<button onClick={this.goBack}>Back</button>
+						<FontAwesomeIcon onClick={this.goBack} icon={faCaretLeft} size="9x" className="arrows" color="white" />
+						{/* <button onClick={this.goBack}>Back</button> */}
 						<div classname="details">
 							<h3>Description</h3>
 							<p>Some details go here</p>
 						</div>
-						<button onClick={this.goForward}>Forward</button>
+						<FontAwesomeIcon className="arrows" onClick={this.goForward} icon={faCaretRight} size="9x" color="white" />
+						{/* <button onClick={this.goForward}>Forward</button> */}
 					</div>
 				</div>
 				{/* <RandomButton array={[1, 2, 3, 4, 5, 6]} /> */}
